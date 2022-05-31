900eaa8cdb45345f4777cd13dbbef565c117c6ea
arnort20 <arnort20@ru.is>
2022-05-31 12:45:06 +0000

    friendship established

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+          has_many :friends, class_name: "friend", foreign_key: "reference_id"
 end
